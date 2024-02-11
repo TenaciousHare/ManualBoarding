420c1bbed1fdbb98a04279188c6b901c688f8ee0
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2024-02-11 23:08:17 +0100

    More tests added, added new custom hook useCountZones, extracted from useSeatMap, refactor in Totals component

--- src/components/Gap/Gap.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./Gap.module.css";
-
-export const Gap = () => {
-  return <td role="separator" className={styles.gap}></td>;
-};
--- src/components/Wrapper/Wrapper.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./Wrapper.module.css";
-import { WrapperProps } from "../../types/interfaces";
-
-export const Wrapper = ({ children, isColumn = false }: WrapperProps) => {
-  return (
-    <div className={isColumn ? styles.column : styles.wrapper}>{children}</div>
-  );
-};
