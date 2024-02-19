786212574de6e7283bb5c546401df54ca904d73a
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2024-02-19 08:14:20 +0100

    Testing terser minify options

--- src/store/generateSeatMap/helpers/compose.ts
+++
@@ -1,4 +0,0 @@
-export const compose = <T>(...fns: Array<(arg: T) => T>): ((arg: T) => T) => {
-  return (x: T) =>
-    fns.reduceRight((acc: T, fn: (arg: T) => T): T => fn(acc), x);
-};
--- src/store/generateSeatMap/helpers/generateSeqNumbersArray.ts
+++
@@ -1,7 +0,0 @@
-export function generateSeqNumbersArray(seq: number): number[] {
-  const seqArr = [];
-  for (let i = 1; i <= seq; i++) {
-    seqArr.push(i);
-  }
-  return seqArr;
-}
